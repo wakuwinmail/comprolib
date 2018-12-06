4e61f9480afa81d506429b93b54376fcc1472b8c
MiuraWakuya <wakuwinmail@yahoo.co.jp>
2018-12-06 17:15:44 +0900

    change:add operator typename

--- lazysegtree.cpp
+++ lazysegtree.cpp
@@ -4,34 +4,38 @@
 #include <algorithm>
 #include <functional>
 
-template <typename T>
+template <typename T,typename E>
 struct LazySegmentTree{
 private:
     typedef std::function<T(T,T)> F;
-    typedef std::function<T(T,int)> P;
+    typedef std::function<T(T,E)> G;
+    typedef std::function<E(E,E)> H;
+    typedef std::function<E(E,int)> P;
     int n;
     T init;
+    E opinit;
     F f;//function for caliculate
-    F g;//function for update
-    F h;//function for evaluate
+    G g;//function for update
+    H h;//function for evaluate
     P p;//function for range calculate
     std::vector<T> node;
     std::vector<T> lazy;
 public:
-    explicit LazySegmentTree(int sz,F cal,F upd,F ecal,P rcal=[](T a,int b){return a;},T initv=0){
+    explicit LazySegmentTree(int sz,F cal,G upd,H ecal,P rcal=[](T a,int b){return a;},T initv=0,E opinitv=0){
         n=1;
         f=cal;
         g=upd;
         h=ecal;
         p=rcal;
         init=initv;
+        opinit=opinitv;
         while(n<sz)n=n*2;
         node.resize(static_cast<unsigned int>(2 * n - 1), init);
         for (int i = 0; i <sz ; ++i) node[i+n-1]=init;
         for (int i = n-2; i >= 0 ; --i) node[i]=f(node[2*i+1],node[2*i+2]);
 
         lazy.resize(static_cast<unsigned int>(2 * n - 1), init);
-        for (int i = 0; i <sz ; ++i) lazy[i+n-1]=init;
+        for (int i = 0; i <sz ; ++i) lazy[i+n-1]=opinit;
         for (int i = n-2; i >= 0 ; --i) lazy[i]=f(lazy[2*i+1],lazy[2*i+2]);
     }
 
@@ -62,24 +66,25 @@ public:
     }
 
     void eval(int k,int len){//k:0-indexed
-        if(lazy[k]==init)return;
+        if(lazy[k]==opinit)return;
         node[k]=g(node[k],p(lazy[k],len));
         if(k<n-1){
                 lazy[2*k+1]=h(lazy[2*k+1],lazy[k]);
                 lazy[2*k+2]=h(lazy[2*k+2],lazy[k]);
         }
-        lazy[k]=init;
+        lazy[k]=opinit;
     }
 };
 
 void solve(){
     int n,q;
     std::cin>>n>>q;
-    LazySegmentTree<long> st(n,
+    LazySegmentTree<long,long> st(n,
         std::plus<long>(),
         std::plus<long>(),
         std::plus<long>(),
         std::multiplies<long>(),
+        0L,
         0L
     );
     for(size_t i = 0; i < q; i++)
