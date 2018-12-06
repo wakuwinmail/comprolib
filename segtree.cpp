ca8ba976d7a24754414d5b670163bb4e64f1296c
MiuraWakuya <wakuwinmail@yahoo.co.jp>
2018-12-06 15:27:55 +0900

    add,rename:add abstacted segtree, rename lazy segtree

---
+++ segtree.cpp
@@ -0,0 +1,71 @@
+//https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/all/DSL_2_A
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdint>
+#include <functional>
+
+template <typename T>
+struct SegmentTree{
+private:
+    typedef std::function<T(T,T)> F;
+    int n;
+    T init;
+    F f;
+    std::vector<T> node;
+ 
+public:
+    explicit SegmentTree(int sz,F func,T initv=std::numeric_limits<T>::max()){
+        n=1;
+        init=initv;
+        f=func;
+        while(n<sz)n=n*2;
+        node.resize(static_cast<unsigned int>(2 * n - 1), init);
+        for (int i = 0; i <sz ; ++i) node[i+n-1]=init;
+        for (int i = n-2; i >= 0 ; --i) node[i]=f(node[2*i+1],node[2*i+2]);
+    }
+ 
+    void update(int x, T val){//x:0-indexed
+        x+=n-1;
+        node[x]=val;
+        while(x>0){
+            x=(x-1)/2;
+            node[x]=f(node[2*x+1],node[2*x+2]);
+        }
+    }
+ 
+    T query(int p,int q,int k=0,int l=0,int r=-1){//[p,q):0-indexed
+        if(r<0)r=n;
+        if(r<=p||l>=q)return init;//out of range
+        if(p<=l&&r<=q)return node[k];
+ 
+        return f(query(p,q,2*k+1,l,(l+r)/2),query(p,q,2*k+2,(l+r)/2,r));
+    }
+};
+
+void solve(){
+    int n,q;
+    std::cin>>n>>q;
+    SegmentTree<int> st(n,
+        [](int a,int b){return std::min(a,b);},//example for RMQ
+        INT32_MAX
+    );
+    for(size_t i = 0; i < q; i++)
+    {
+        int com,x,y;
+        std::cin>>com>>x>>y;
+        if(com==0)//update
+        {
+            st.update(x,y);
+        }
+        else //find
+        {
+            std::cout<<st.query(x,y+1)<<std::endl;
+        }
+    }
+}
+
+int main(){
+    solve();
+    return 0;
+}
