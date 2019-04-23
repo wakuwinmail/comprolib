6bdd24eb0bc93af4e00e4cd1dc01ac177f10cbce
wakuwinmail <wakuwinmail@yahoo.co.jp>
2019-04-23 12:54:02 +0900

    change:gdcとlcmを関数だけにした（単体でコンパイルが通らない）

--- math/lcm.cpp
+++ math/lcm.cpp
@@ -1,27 +1,7 @@
-#include <iostream>
-
-template<typename T>
-T gcd(T a,T b){
-    if(b==0)return a;
-    return gcd(b,a%b);
-}
-
-//GCDが必要です
-//cut begin
+#include "gcd.cpp"
+//GCDが必要
 template<typename T>
 T lcm(T a,T b){
     T d=gcd(a,b);
     return (a/d)*b;
-}
-//cut end
-
-void solve(){
-    int a,b;
-    std::cin>>a>>b;
-    std::cout<<lcm(a,b)<<std::endl;
-}
-
-int main(){
-    solve();
-    return 0;
 }
