166e046af743296e5cdfdd69be86358734a87f7c
wakuwinmail <wakuwinmail@yahoo.co.jp>
2019-05-07 12:44:22 +0900

    add:modpowを追加

--- math/modpow.cpp
+++ math/modpow.cpp
@@ -1,9 +1,19 @@
+#include <iostream>
+//cut begin
 template<typename T>
 T modpow(T a,T n,T mod){
     T ret=1;
     while(n>0){
         if((n&1)!=0){//n%2==1
-
+            ret=ret*a%mod;
         }
+        a=a*a%mod;
+        n=n/2;
     }
+    return ret;
+}
+//cut end
+int main(){
+    std::cout<<modpow(3,45,1000000007)<<std::endl;
+    return 0;
 }
