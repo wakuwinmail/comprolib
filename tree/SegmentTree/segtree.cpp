07958f15c5d93963489ba8773359205e63bbdff1
wakuwinmail <wakuwinmail@yahoo.co.jp>
2019-08-27 23:17:01 +0900

    change,add:セグ木を高度抽象化版と普通抽象化版に分けた

--- tree/SegmentTree/segtree.cpp
+++ tree/SegmentTree/segtree.cpp
@@ -4,74 +4,61 @@
 #include <algorithm>
 #include <cstdint>
 #include <functional>
-#include "monoid.cpp"
 
-//cut begin
-template <typename T,typename E=T>
+template <typename T,typename E>
 struct SegmentTree{
 private:
-    using G=std::function<T(T,E)>;
+    typedef std::function<T(T,T)> F;
+    typedef std::function<T(T,E)> G;
     int n;
     std::vector<T> init;
-    monoid<T> mono;
-    G upd;//function for update
+    F f;//function for caliculate
+    G g;//function for update
     std::vector<T> node;
 
 public:
     explicit SegmentTree(
             int sz,
-            monoid<T> mono,
-            G upd
-    ):mono(mono),upd(upd)
-    {
-        n=1;
-        while(n<sz)n=n*2;
-        node.resize(static_cast<unsigned int>(2 * n - 1), mono.get_id());
-        for (int i = 0; i <sz ; ++i) node[i+n-1]=mono.get_id();
-        for (int i = n-2; i >= 0 ; --i) node[i]=mono(node[2*i+1],node[2*i+2]);
-    }
-
-    explicit SegmentTree(
-            int sz,
-            monoid<T> mono,
+            F cal,
             G upd,
-            std::vector<T> init
-    ):mono(mono),upd(upd),init(init)
-    {
+            std::vector<T> initv=std::vector<T>(1,0)
+    ){
         n=1;
+        init=initv;
+        f=cal;
+        g=upd;
         while(n<sz)n=n*2;
-        node.resize(static_cast<unsigned int>(2 * n - 1), mono.get_id());
+        node.resize(static_cast<unsigned int>(2 * n - 1), init[0]);
         for (int i = 0; i <sz ; ++i) node[i+n-1]=init[i+1];
-        for (int i = n-2; i >= 0 ; --i) node[i]=mono(node[2*i+1],node[2*i+2]);
+        for (int i = n-2; i >= 0 ; --i) node[i]=f(node[2*i+1],node[2*i+2]);
     }
 
     void update(int x, E val){//x:0-indexed
         x+=n-1;
-        node[x]=upd(node[x],val);
+        node[x]=g(node[x],val);
         while(x>0){
             x=(x-1)/2;
-            node[x]=mono(node[2*x+1],node[2*x+2]);
+            node[x]=f(node[2*x+1],node[2*x+2]);
         }
     }
 
     T query(int p,int q,int k=0,int l=0,int r=-1){//[p,q):0-indexed
         if(r<0)r=n;
-        if(r<=p||l>=q)return mono.get_id();//out of range
+        if(r<=p||l>=q)return init[0];//out of range
         if(p<=l&&r<=q)return node[k];
 
-        return mono(query(p,q,2*k+1,l,(l+r)/2),query(p,q,2*k+2,(l+r)/2,r));
+        return f(query(p,q,2*k+1,l,(l+r)/2),query(p,q,2*k+2,(l+r)/2,r));
     }
 };
-//cut end
+
 void solve(){
     int n,q;
     std::cin>>n>>q;
-    monoid<int> mono([](int a,int b){return std::min(a,b);},std::numeric_limits<int>::max());
-    SegmentTree<int> st(n,
-        mono,
+    SegmentTree<int,int> st(n,
+        [](int a,int b){return std::min(a,b);},//example for RMQ
         [](int a,int b){return b;}
     );
-    for(int i = 0; i < q; i++)
+    for(size_t i = 0; i < q; i++)
     {
         int com,x,y;
         std::cin>>com>>x>>y;
